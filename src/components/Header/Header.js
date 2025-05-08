0d67fadd492543c49958037bcb8b0e21adf5c2f9
AmirHAta <kianebtekari0@gmail.com>
2025-05-08 18:48:04 +0330

    [Fix]: Finished tailwind instalation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <div className='px-28 pt-11'>
+    <div className='px-28 pt-11 '>
       <div className='flex items-center justify-between h-14'>
         <h1>Logo</h1>
         <ul className='inline-flex gap-6'>
