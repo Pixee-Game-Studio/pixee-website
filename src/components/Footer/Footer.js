a974974c5d906dd2790ef8eefd99e4868f860f59
Kian <kianebtekari0@gmail.com>
2025-06-28 16:45:33 +0330

    [fix]: create footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,6 +2,33 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <footer className='bg-black w-full mt-40'>
+      <div className='container'>
+        <div className='flex items-center flex-wrap py-16 gap-5 sm:gap-10 justify-between'>
+          <div>
+            <h1 className='text-white text-xl font-BebasNeueRegular sm:text-2xl lg:text-4xl'>PIXEE</h1>
+            <p className='text-primary-color font-TestSohneMono text-xs sm:text-sm lg:max-w-[400px] xl:max-w-[500px] leading-relaxed lg:text-base'>pixee Game is an independent game studio driven by creativity and passion, crafting unique and engaging experiences for players around the world.</p>
+            <p className='text-secondery-color font-TestSohneMono'>get it on system</p>
+          </div>
+          <div>
+            <p className='text-secondery-color text-sm font-BebasNeueRegular sm:text-base lg:text-lg'>CONTACT US</p>
+            <h1 className='text-white text-xl font-BebasNeueRegular sm:text-2xl'>Here, every email could be the beginning of <br /> something unforeseen.</h1>
+            <div className='text-white mt-5 font-TestSohneMono text-xs sm:text-sm lg:text-base'>
+              <h1>Email Studio : pixeestudio@gmail.com</h1>
+              <h1>Location : Iran,Tehran</h1>
+            </div>
+          </div>
+        </div>
+        <hr className='w-full text-primary-color' />
+        <div className='flex items-center flex-wrap gap-3 justify-between px-5 py-5'>
+          <p className='text-white text-xs sm:text-sm lg:text-base'>©2024 Pixee Game</p>
+          <div className='flex items-center justify-center gap-5 sm:gap-10'>
+            <p className='font-IMBPlex text-primary-color text-xs sm:text-sm lg:text-base'>Terms of Service</p>
+            <p className='font-IMBPlex text-primary-color text-xs sm:text-sm lg:text-base'>Privacy</p>
+            <p className='font-IMBPlex text-primary-color text-xs sm:text-sm lg:text-base'>Cookies</p>
+          </div>
+        </div>
+      </div>
+    </footer>
   )
 }
