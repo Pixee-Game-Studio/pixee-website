3bf7a115e6581194468e417fbec3e7d440a32755
Kian <kianebtekari0@gmail.com>
2025-06-29 10:34:44 +0330

    [fix]: responsive the footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,11 +4,11 @@ export default function Footer() {
   return (
     <footer className='bg-black w-full mt-40'>
       <div className='container'>
-        <div className='flex items-center flex-wrap py-16 gap-5 sm:gap-10 justify-between'>
+        <div className='flex items-center flex-wrap sm:pb-16 lg:px-14 py-6 sm:pt-16 lg:pt-24 gap-5 sm:gap-10 justify-between'>
           <div>
             <h1 className='text-white text-xl font-BebasNeueRegular sm:text-2xl lg:text-4xl'>PIXEE</h1>
             <p className='text-primary-color font-TestSohneMono text-xs sm:text-sm lg:max-w-[400px] xl:max-w-[500px] leading-relaxed lg:text-base'>pixee Game is an independent game studio driven by creativity and passion, crafting unique and engaging experiences for players around the world.</p>
-            <p className='text-secondery-color font-TestSohneMono'>get it on system</p>
+            <a href='#' className='text-secondery-color font-TestSohneMono mt-2'>get it on system</a>
           </div>
           <div>
             <p className='text-secondery-color text-sm font-BebasNeueRegular sm:text-base lg:text-lg'>CONTACT US</p>
@@ -19,9 +19,9 @@ export default function Footer() {
             </div>
           </div>
         </div>
-        <hr className='w-full text-primary-color' />
-        <div className='flex items-center flex-wrap gap-3 justify-between px-5 py-5'>
-          <p className='text-white text-xs sm:text-sm lg:text-base'>©2024 Pixee Game</p>
+        <hr className='w-full border-[#717171]' />
+        <div className='flex items-center flex-wrap gap-3 justify-between lg:px-14 py-5'>
+          <p className='text-white text-xs sm:text-sm lg:text-base font-IMBPlex'>©2024 Pixee Game</p>
           <div className='flex items-center justify-center gap-5 sm:gap-10'>
             <p className='font-IMBPlex text-primary-color text-xs sm:text-sm lg:text-base'>Terms of Service</p>
             <p className='font-IMBPlex text-primary-color text-xs sm:text-sm lg:text-base'>Privacy</p>
